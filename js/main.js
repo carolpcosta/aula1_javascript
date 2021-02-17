95b4147019217f14d3deb5ef536008c03cc6622b
carollineargo <carolline.costa@useargo.com>
2021-02-17 12:17:22 -0300

    Introdução a comandos básicos para js.

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var nome = "Carol";
+var idade  = 25;
+alert(nome + " tem " + idade + " anos");
+
+//para evitar de usar alert, usar console.log
+//console.log(nome);
+//console.log(idade);
+
+//arrays
+var lista = ["maçã", "pera", "laranja"];
+alert(lista);
+//lista.pop() -> retira ultimo elemento
+//lista.push(uva) -> adiciona elemento 
+//list.lenght -> tamanho da lista
+//lista.join(" - ") -> troca o separador da lista
+
+
+//dicionario -> é como se fosse um JSON
+//var fruta = {nome: "maçã", cor:"vermelha"};
+
+//entrada de dados
+//var idade = prompt("Qual a sua idade?");
+
+//condicionais
+/*
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+ */
+
+ //date
+ //var d = new Date();
+ //alert(d.getMinutes()); -> day, month, hours...
+
+ //funções
+ /*function soma(n1, n2){
+     return n1 + n2;
+ }
+ alert(soma(5,10));
+ */
